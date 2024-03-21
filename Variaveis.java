09e4e2224fcfc35663eda96d55943df3cfababcd
Vanderlei Silva <vanjosil@gmail.com>
2024-03-20 22:23:47 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,32 @@
+/**
+* Classe para explicação de variáveis e suas regras
+* @author Kaio
+* @version 1.0
+*/
+public class Variaveis{
+    
+    final static String BEM_VINDO = "Bem vindo ";
+    
+    /**
+    * Método main para execução
+    */
+    public static void main(String[ ] args){
+        String nome = "Kaio";
+        System.out.println(BEM_VINDO + nome);
+               
+        String nome1 = "";
+        
+        /*
+        {
+            //nome = "Fulano";
+            System.out.println(BEM_VINDO + nome);
+            
+            nome1 = "G2";
+            
+        }
+        */
+        System.out.println(nome1);
+        
+    }
+    
+}
