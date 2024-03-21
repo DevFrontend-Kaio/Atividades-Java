e5829729a7c64f9711c5baaff80d18507ca3c338
Vanderlei Silva <vanjosil@gmail.com>
2024-03-20 22:24:46 -0300

    Add files via upload

--- Operadores.java
+++ Operadores.java
@@ -1,59 +1,75 @@
 public class Operadores{
-    public static void main(String[ ] args){
-        // + - / * %
-        int nr1 = 3 + 4 + 5;
-        System.out.println(nr1);
-        System.out.println(10 - 6);
-        System.out.println(10 / 2);
-        System.out.println(7 * 6);
-        System.out.println(11 % 2);
+    public static void main(String[ ] args){ 
         
-        int nr2 = 5 + 4 * 2;
-        System.out.println(nr2);
+        // < > <= >= == !=
         
-        int nr3 = (5 + 4) * 2;
-        System.out.println(nr3);
+        int a = 10;
+        int b = 15;
         
-        int nr4 = (5 + 4) * 2 / 2;
-        System.out.println(nr4);
+        boolean res = a < b;
+        System.out.println(res);
         
-        System.out.println(3 + 4 + 5);
-        System.out.println("O resultado é: " + 3 + 4 + 5 + "texto");
+        System.out.println(a > 5); //true
+        System.out.println(a <= 10); //true
+        System.out.println(5 >= 6); //false
+        System.out.println(5 >= 5); //true
+        System.out.println(b == 15); //true
+        System.out.println(b != 15); //false
         
-        double saldo = 100.00;
-        //saldo = saldo + 200.00;
-        saldo += 200.0;
-        System.out.println(saldo);
         
-        saldo -= 50.0;
-        System.out.println(saldo);
+         System.out.println("----------------");
+         System.out.println("Operadores Lógicos");
         
-        saldo /= 5;
-        System.out.println(saldo);
+        // &&
+        // 10 == 10 && 15 == 15
+        // true && true -> trye
+        System.out.println(a ==10 && b == 15); //true
+        System.out.println(a ==11 && b == 15); //false
+        System.out.println(a ==10 && b == 14); //false
+        System.out.println(a ==11 && b == 14); //false
         
-        saldo *= 10;
-        System.out.println(saldo);
+        System.out.println(a ==10 && b == 15 && 60 ==60); //true
         
-        saldo %= 10;
-        System.out.println(saldo);
+         System.out.println("----------------");
+         System.out.println("Operadores Lógicos");
+         System.out.println(" Ou ");
         
-         
-        System.out.println("------------------------------");
+        // &&
+        // 10 == 10 && 15 == 18
+        // true && false -> true
+        System.out.println(a == 10 || b == 15); //true
+        System.out.println(a == 11 || b == 15); //true
+        System.out.println(a == 10 || b == 14); //true
+        System.out.println(a == 11 || b == 14); //false
         
-        // ++ --
+        System.out.println(a ==11 || b == 20 || 60 == 60); //true
+        System.out.println(a ==11 || b == 20 || 60 == 61); //false
         
-        int contador = 0;
-        contador++; 
-        System.out.println(contador);
-        contador--; 
-        System.out.println(contador);
+        System.out.println("----------------");
+        System.out.println("Operadores Lógicos");
+        System.out.println(" ! ");
         
-        System.out.println("------------------------------");
-        //contador++
-        //++contador
-        int contador2 = 0; 
-        //System.out.println(contador2++);
-        System.out.println(++contador2);
+        System.out.println(!(a ==10) && b == 15); //false
+        System.out.println(!(a ==10 && b == 15)); //false
+        System.out.println(!(a ==11 && b == 16)); //true
         
+        //Precedência ! && || 
+          
+        System.out.println("----------------");
+        boolean c1 = 2 > 3 || 4 != 5; // true
+        boolean c2 = !(2 > 3) && 4 != 5; // true
+        boolean c3 = 10 < 5; // false
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        
+        System.out.println("----------------"); 
+        boolean c4 = c1 || c2 && c3; // true
+            // c2 && c3 -> true && false = false
+            // c1 || false    
+        System.out.println(c4);  
+        
+        boolean c5 = c1 && c2 && !(c3); 
+        System.out.println(c5);
     }
 }
